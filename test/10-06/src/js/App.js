1c776677c59d4a48479bb6e94fefb46ce2d46651
YamanKatmawi <yamankatmawi@gmx.de>
2020-06-10 16:54:33 +0200

    try agin to build in React

---
+++ test/10-06/src/js/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Text from "./Text";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi I am React</h1>
+      <Text />
+    </div>
+  );
+};
+
+export default App;
