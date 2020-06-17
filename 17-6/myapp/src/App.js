7d41900fe778360753ff7802788624d829a4d165
YamanKatmawi <yamankatmawi@gmx.de>
2020-06-17 11:18:21 +0200

    regex

---
+++ 17-6/myapp/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      result: "",
+    };
+    this.handleChange = (e) => {
+      this.setState({
+        userInput: e.target.value,
+      });
+    };
+    this.validate = (email) => {
+      const regex = /^[A-Za-z\.\-\_]+@\w+\.[A-Za-z]{1,6}$/;
+      return regex.test(email);
+    };
+    this.handleSubmit = (e) => {
+      e.preventDefault();
+      const entry = this.state.userInput;
+      this.setState({
+        result: this.validate(entry) ? "valid" : "invalid",
+      });
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hi I am React</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.userInput}
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="check" />
+        </form>
+        <div>Your Email is {this.state.result}</div>
+      </React.Fragment>
+    );
+  }
+}
